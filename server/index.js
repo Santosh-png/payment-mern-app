67c1895da7e708ac403e727dbaaad8ba6d2836aa
unknown <santoshbist69@gmail.com>
2021-03-30 20:24:58 +0545

    install nodemon

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.get('/',(req,res) =>{
-    res.send("I am running well ato port 5000.Hello sir!");
+    res.send("I am for port  4000 on local machine too.");
 })
 app.listen(PORT,
-    console.log("I am running at port 5000. Hello ha ha ha .")
+    console.log("I am running at port 4000.")
     );
