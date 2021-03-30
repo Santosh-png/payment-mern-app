a8ae46ce38bb7fb7b652a68a69b044166b7558f3
unknown <santoshbist69@gmail.com>
2021-03-30 13:30:08 +0545

    deployment on heroku

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res) =>{
-    res.send("I am running well ato port 5000");
+    res.send("I am running well ato port 5000.Hello sir!");
 })
-app.listen(5000,
-    console.log("I am running at port 5000.")
+app.listen(PORT,
+    console.log("I am running at port 5000. Hello ha ha ha .")
     );
