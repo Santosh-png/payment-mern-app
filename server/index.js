c3df3fa8426d02967cb38e0d6004b3f71c134e71
unknown <santoshbist69@gmail.com>
2021-04-01 15:50:24 +0545

    google strategy options

--- server/index.js
+++ server/index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('config/keys');
 const PORT = process.env.PORT || 4000;
 
 app.get('/',(req,res) =>{
     res.send("I am for port  4000 on local machine too.");
 })
-//Client ID : 67395368495-0lj1ve7rfvvc5q2otssaq89a09usqdba.apps.googleusercontent.com
-//Client Secret : sodb-SPYdhGNNvKySEWb68-M
-passport.use(new GoogleStrategy({}));
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/callback"
+}, (accessToken) => {
+    console.log(accessToken);
+})
+);
 
 
 app.listen(PORT,
