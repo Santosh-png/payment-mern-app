99c40953219f30521712d8e3f5a5eb5d708c3981
unknown <santoshbist69@gmail.com>
2021-04-01 17:09:43 +0545

    Testing oauth

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('config/keys');
+const keys = require('./config/keys');
 const PORT = process.env.PORT || 4000;
 
 app.get('/',(req,res) =>{
@@ -10,15 +10,23 @@ app.get('/',(req,res) =>{
 })
 
 passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback"
-}, (accessToken) => {
-    console.log(accessToken);
-})
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
 );
 
-
+app.get(
+    '/auth/google',
+    passport.authenticate(
+        'google',
+        {
+            scope: ['profile','email']
+        }
+    )
+);
 app.listen(PORT,
     console.log("I am running at port 4000.")
     );
